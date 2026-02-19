8cff0a29415ea1a989aa1e9fcff94884a72919fa
neumanncomputer <yusuke.official.email@gmail.com>
2026-02-19 11:10:53 +0900

    uvに変更したためpyenvを削除

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,8 +10,3 @@ starship init fish | source
 # Node.jsバージョン管理ツールの初期化
 set -gx VOLTA_HOME "$HOME/.volta"
 set -gx PATH "$VOLTA_HOME/bin" $PATH
-
-# Pythonバージョン管理ツールの初期化
-set -Ux PYENV_ROOT $HOME/.pyenv
-test -d $PYENV_ROOT/bin; and fish_add_path $PYENV_ROOT/bin
-pyenv init - fish | source
